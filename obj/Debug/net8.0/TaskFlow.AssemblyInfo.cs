6f58a86a9630dc26c5e0d1b97f854a8e38de0f38
Ahmed Abdelbarr <121731712+ahmedhass349@users.noreply.github.com>
2026-04-06 13:31:04 +0200

    rolled back to a more stable version

--- obj/Debug/net8.0/TaskFlow.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taskflow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61eb2854350e3e2cbfb9d049b2b68135ef5f3ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa663c291822209a22a371a0b0438d5a19ea013")]
 [assembly: System.Reflection.AssemblyProductAttribute("taskflow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taskflow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
